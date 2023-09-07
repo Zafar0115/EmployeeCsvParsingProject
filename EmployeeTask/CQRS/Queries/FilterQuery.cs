e52e733a3f5b65bb58f59f5d1c611e8af786df83
Zafarbek <=>
2023-09-07 18:50:49 +0500

    Pagination logic

--- EmployeeTask/CQRS/Queries/FilterQuery.cs
+++ EmployeeTask/CQRS/Queries/FilterQuery.cs
@@ -4,47 +4,69 @@ using MediatR;
 
 namespace EmployeeTask.CQRS.Queries
 {
-    public class FilterQuery:IRequest<IEnumerable<Employee>?>
+    public class FilterQuery : IRequest<IEnumerable<Employee>?>
     {
+        const int maxPageSize = 50;
         public string sortingOrder { get; set; } = "asc";
         public string search { get; set; } = "";
-    }
-
-    public class FilterQueryHandler : IRequestHandler<FilterQuery, IEnumerable<Employee>?>
-    {
-        private readonly ApplicationDbContext _dbContext;
-
-        public FilterQueryHandler(ApplicationDbContext dbContext)
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize=10;
+        public int PageSize
         {
-            _dbContext = dbContext;
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = value > 50 ? maxPageSize : value;
+            }
+
         }
 
-        public Task<IEnumerable<Employee>?> Handle(FilterQuery request, CancellationToken cancellationToken)
+        public class FilterQueryHandler : IRequestHandler<FilterQuery, IEnumerable<Employee>?>
         {
-            IEnumerable<Employee> filteredEmployees = _dbContext.Employees;
+            private readonly ApplicationDbContext _dbContext;
 
-            if (request.search != null&& request.search!="")
+            public FilterQueryHandler(ApplicationDbContext dbContext)
             {
-                request.search = request.search.ToLower();
-                filteredEmployees = filteredEmployees
-                .Where(emp =>
-                 emp.PayyrollNumber.ToLower().Contains(request.search) ||
-                 emp.Forename.ToLower().Contains(request.search) ||
-                 emp.Surname.ToLower().Contains(request.search) ||
-                 emp.StartDate.ToLower().Contains(request.search) ||
-                 emp.Address.ToLower().Contains(request.search) ||
-                 emp.Address2.ToLower().Contains(request.search) ||
-                 emp.Telephone.ToLower().Contains(request.search) ||
-                 emp.Postcode.ToLower().Contains(request.search) ||
-                 emp.EmailHome.ToLower().Contains(request.search) ||
-                 emp.Mobile.ToLower().Contains(request.search));
+                _dbContext = dbContext;
             }
-            if (filteredEmployees is not null)
-                filteredEmployees = request.sortingOrder.ToLower() == "desc" ? filteredEmployees
-                                   .OrderByDescending(emp => emp.Surname) :
-                                   filteredEmployees.OrderBy(emp => emp.Surname);
 
-            return Task.FromResult(filteredEmployees);
+            public Task<IEnumerable<Employee>?> Handle(FilterQuery request, CancellationToken cancellationToken)
+            {
+                IEnumerable<Employee> filteredEmployees = _dbContext.Employees;
+
+                if (request.search != null && request.search != "")
+                {
+                    request.search = request.search.ToLower();
+                    filteredEmployees = filteredEmployees
+                    .Where(emp =>
+                     emp.PayyrollNumber.ToLower().Contains(request.search) ||
+                     emp.Forename.ToLower().Contains(request.search) ||
+                     emp.Surname.ToLower().Contains(request.search) ||
+                     emp.StartDate.ToLower().Contains(request.search) ||
+                     emp.Address.ToLower().Contains(request.search) ||
+                     emp.Address2.ToLower().Contains(request.search) ||
+                     emp.Telephone.ToLower().Contains(request.search) ||
+                     emp.Postcode.ToLower().Contains(request.search) ||
+                     emp.EmailHome.ToLower().Contains(request.search) ||
+                     emp.Mobile.ToLower().Contains(request.search));
+                }
+                if (filteredEmployees is not null)
+                {
+                    filteredEmployees = request.sortingOrder.ToLower() == "desc" ? filteredEmployees
+                                      .OrderByDescending(emp => emp.Surname) :
+                                      filteredEmployees.OrderBy(emp => emp.Surname);
+
+                   filteredEmployees= filteredEmployees.Skip((request.PageNumber - 1) * request.PageSize)
+                                     .Take(request.PageSize);
+                }
+                   
+
+
+                return Task.FromResult(filteredEmployees);
+            }
         }
     }
 }
